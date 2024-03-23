f31232ce2cb9bc2b9ce476de88277302ff9e67be
Nikita <getingjagare@gmail.com>
2024-03-23 23:09:42 +0100

    app: start

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <header>
+        <div class="wrapper"></div>
+    </header>
+
+    <RouterView />
+</template>
+
+<style scoped>
+
+</style>
